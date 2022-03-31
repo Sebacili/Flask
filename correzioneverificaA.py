eff1fc774c90c518af5c62db4c575e3af5ba9cec
Cilibeanu Sebastian <cilibeanu.nicolae@iisgalvanimi.edu.it>
2022-03-31 09:50:10 +0000

    verifica

--- correzioneverificaA.py
+++ correzioneverificaA.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,send_file,make_response, url_for, Response,redirect
 app = Flask(__name__)
 import io
 import geopandas
@@ -12,13 +12,39 @@ import pandas as pd
 stazioni = pd.read_csv("/workspace/Flask/coordfix_ripetitori_radiofonici_milano_160120_loc_final.csv" ,sep = ";")
 @app.route('/', methods=['GET'])
 def home():
-    return render_template("home.html")
+    return render_template("homeverifica1.html")
 
-@app.route('/numero ', methods=['GET'])
+@app.route('/numero', methods=['GET'])
 def numero():
-#numero stazioni per ogni munic
+#numero stazioni per ogni municio
+    global risultato
     risultato = stazioni.groupby("MUNICIPIO")["OPERATORE"].count().reset_index()
-    return render_template("elenco1.html",risultato = risultato.to_html())
+    return render_template("link1.html",risultato = risultato.to_html())
 
+@app.route('/grafico', methods=['GET'])
+def grafico():
+    
+    fig, ax = plt.subplots(figsize = (12,8))
+    x = risultato.MUNICIPIO
+    y = risultato.OPERATORE
+    ax.bar(x,y,color = "#304C89")
+    plt.xlabel("MUNICIPIO")
+    plt.ylabel("OPERATORE")
+
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+@app.route('/selezione', methods=['GET'])
+def selezione():
+    scelta = request.args["scelta"]
+    if scelta == "es1":
+        return redirect(url_for("numero"))
+    elif scelta == "es2":
+        return redirect(url_for("input"))
+    else:
+        return redirect(url_for("dropdown"))
+    return render_template("a.html")
+    
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3245, debug=True)
